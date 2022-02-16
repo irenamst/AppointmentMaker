69655150579cc95470a9c7230c1df552ffccfca0
Irena Staneva <irenamst@gmail.com>
2022-02-16 10:45:14 +0200

    Add files via upload

---
+++ Models/AppointmentModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppointmentMaker.Models
+{
+    public class AppointmentModel
+    {
+        [Required]
+        [StringLength(20,MinimumLength =4)]
+        [DisplayName("Patiant's full name")]
+        public string PatiantName { get; set; }
+        [DisplayName("Appointment Request Date")]
+        [DataType(DataType.Date)]
+        public DateTime AppointmentDate { get; set; }
+        [DisplayName("Patiant Approximate Net Worth")]
+        [DataType(DataType.Currency)]
+        public decimal PatiantNetWorth { get; set; }
+        [DisplayName("Primary Doctor's Last Name")]
+        public string DoctorName { get; set; }
+        [DisplayName("Patient's Perceived Level of Pain (1 low to 10 high)")]
+        [Range(1,10)]
+        public int PainLevel { get; set; }
+    }
+}
